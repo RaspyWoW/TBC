ba9f9e2f499ad83e28a4005554181218f2ea978f
Nolin7777 <nolin.nolin.nolin.nolin@gmail.com>
2022-01-10 12:44:03 +0100

    Add forgotten loading to GameObjectArtKit.dbc
    credits cmangos

--- src/shared/revision_sql.h
+++ src/shared/revision_sql.h
@@ -1,7 +1,6 @@
-#ifndef __REVISION_SQL_H__
-#define __REVISION_SQL_H__
+#pragma once
+
  #define REVISION_DB_REALMD "required_s2433_01_realmd_anticheat"
  #define REVISION_DB_LOGS "required_s2433_01_logs_anticheat"
  #define REVISION_DB_CHARACTERS "required_s2429_01_characters_raf"
- #define REVISION_DB_MANGOS "required_s2439_01_mangos_groups_formation"
-#endif // __REVISION_SQL_H__
+ #define REVISION_DB_MANGOS "required_s2440_01_mangos_artkits"
